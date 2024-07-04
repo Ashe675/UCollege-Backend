7d006daebc8eb2fc748a69b8bf46bf0ff3c44aab
Jose Cerrato <josecerrato675@gmail.com>
2024-07-03 21:54:31 -0600

    test

--- src/routes/resultRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { generateCsv } from '../controllers/resultController';
-
-const router = Router();
-
-router.get('/generate-csv', generateCsv);
-
-export default router;
