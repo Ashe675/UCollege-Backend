9d8cfd508b640185b2d4658738786933989b74fb
Jose Cerrato <josecerrato675@gmail.com>
2024-07-05 02:19:07 -0600

    file name

--- src/controllers/admission/gradeController.ts
+++ src/controllers/admission/gradeController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { CSVService } from "../../services/admission/CSVService";
-import { GradeService } from "../../services/admission/GradeService";
+import { GradeService } from "../../services/admission/gradeService";
 import { sendEmailResults } from "../../services/mail/emailService";
 
 
