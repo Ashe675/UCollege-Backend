fe0ad67373822c2c64f0a2e0dff22dd02ae8df52
VARE-UNAH <hector.varela@unah.hn>
2024-07-05 22:11:51 -0600

    Correción rutas 2

--- src/routes/admission/resultRoutes.ts
+++ src/routes/admission/resultRoutes.ts
@@ -3,6 +3,6 @@ import { generateCsv } from '../../controllers/admission/resultController';
 
 const router = Router();
 
-router.get('/generate-csv', generateCsv);
+router.get('admission/generate-csv', generateCsv);
 
 export default router;
