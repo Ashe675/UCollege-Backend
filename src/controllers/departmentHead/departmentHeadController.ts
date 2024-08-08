584b372c42315a5130fd57da7cd202e19204bd0f
cesarfigueroa503 <cesarfigueroa503@gmail.com>
2024-08-08 04:08:12 -0600

    Servicio para obtener el historial academico, correccion de matricula estudiante

--- src/controllers/departmentHead/departmentHeadController.ts
+++ src/controllers/departmentHead/departmentHeadController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../config/db';
+import e from 'cors';
 
 export const getAcademicHistory = async (req: Request, res: Response) => {
     const { id: userId } = req.user;
@@ -32,6 +33,7 @@ export const getAcademicHistory = async (req: Request, res: Response) => {
                     }
                 },
                 person: true,
+                images: true
                 
             }
         })
@@ -42,6 +44,7 @@ export const getAcademicHistory = async (req: Request, res: Response) => {
         academicHistory['codeIdentification'] = student.identificationCode
 
         academicHistory['globalAverage'] = student.student.globalAverage
+        academicHistory['avatar'] = student.images
 
         //Buscar centro regional
         const regionalCenter_career_User = await prisma.regionalCenter_Faculty_Career_User.findFirst({
@@ -61,20 +64,98 @@ export const getAcademicHistory = async (req: Request, res: Response) => {
             }
         })
 
+
+        
         if (!regionalCenter_career_User) {
             return res.status(404).json({ error: 'No se encontró centro regional del usuario' });
         }
-
-        academicHistory['regionalCenter'] = regionalCenter_career_User.regionalCenter_Faculty_Career.regionalCenter_Faculty.regionalCenter.name
-
-        academicHistory['career'] = regionalCenter_career_User.regionalCenter_Faculty_Career.career.name
+        
+        academicHistory['regionalCenter'] = regionalCenter_career_User.regionalCenter_Faculty_Career.regionalCenter_Faculty.regionalCenter.name;
+        
+        academicHistory['career'] = regionalCenter_career_User.regionalCenter_Faculty_Career.career.name;
+        
 
         
 
+        // Realiza la consulta para obtener las inscripciones
+        const enrollments = await prisma.enrollment.findMany({
+            where: {
+            studentId: student.student.id
+            },
+            include:{
+                section:{
+                    include:{
+                        class:true,
+                        academicPeriod:true
+                    }
+                }
+            }
+        });
+        
+        // Extrae los años de las inscripciones
+        const years = enrollments.map(enrollment => {
+            // Asumiendo que tienes una propiedad 'enrollmentDate' o similar en tu modelo de inscripciones
+            return new Date(enrollment.date).getFullYear();
+        });
+        
+        // Elimina los años duplicados
+        const uniqueYears = [...new Set(years)];
 
-        if (academicHistory.length === 0) {
-            return res.status(404).json({ error: 'No se encontró historial académico para el usuario' });
+
+        academicHistory['years'] = {};
+
+        // Inicializa un array vacío para cada año
+        
+
+        uniqueYears.forEach(year => {
+            academicHistory['years'][year] = {
+              enrollments: [], // Array para almacenar las inscripciones
+              totalAprov: 0    // Inicializar contador de clases aprobadas en 0
+            };
+          });
+      
+        // Llena academicHistory con las clases cursadas en cada año
+        enrollments.forEach(enrollment => {
+            const year = new Date(enrollment.date).getFullYear(); // Obtener el año de la inscripción
+            if (enrollment.grade != null) {
+            const enrollmentDetails = {
+                "codigo": enrollment.section.class.code,
+                "nombre": enrollment.section.class.name,
+                "uv/ca": enrollment.section.class.UV,
+                "periodo": enrollment.section.academicPeriod.number,
+                "Nota": enrollment.grade,
+                "obs": enrollment.grade >= 65 ? "apr" : "rpb"
+            };
+        
+            // Agregar inscripción al array del año correspondiente
+            academicHistory['years'][year].enrollments.push(enrollmentDetails);
+        
+            // Si la inscripción es aprobada, incrementa el contador de clases aprobadas
+            if (enrollmentDetails.obs === "apr") {
+                academicHistory['years'][year].totalAprov++;
+            }
+            }
+        });
+
+        // Variables para almacenar las sumas
+        let totalNotas = 0;
+        let totalUV = 0;
+        academicHistory['SumUVxNota'] = 0;
+        for (let year in academicHistory['years']) {
+            // Recorre cada inscripción en el array 'enrollments' de ese año
+            academicHistory['years'][year].enrollments.forEach(enrollment => {
+                
+                totalUV += enrollment["uv/ca"]; // Suma las UV
+                academicHistory['SumUVxNota'] += enrollment.Nota*enrollment["uv/ca"];
+            });
         }
+
+        academicHistory['SumUV'] = totalUV;
+        academicHistory['academicIndex'] = academicHistory['SumUVxNota']/totalUV;
+        
+
+        
+        
 
         return res.status(200).json(academicHistory);
 
