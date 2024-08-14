1a2b9206663e501e8355109744fce6ef7328a5aa
cesarfigueroa503 <cesarfigueroa503@gmail.com>
2024-08-14 01:09:03 -0600

    Api para exportar pdf y excel lista y probada, flatan validaciones v2

--- src/routes/coordinator/coordinatorRoutes.ts
+++ src/routes/coordinator/coordinatorRoutes.ts
@@ -3,10 +3,10 @@ import { RoleEnum } from '@prisma/client';
 
 //middleware
 import { authenticate, authorizeRole } from '../../middleware/auth/auth';
-import {validateCodeIdentificationData} from '../../middleware/codeIdentification/validateCodeIdentificationData'
-import { exportExcel, exportPdf } from '../../controllers/coordinator/exportAcademicLoadController';
+import {  checkActiveProcessByTypeIdMiddleware } from '../../middleware/checkActiveProcessGeneric';
 
 //controllers
+import { exportExcel, exportPdf } from '../../controllers/coordinator/exportAcademicLoadController';
 
 
 
@@ -15,12 +15,14 @@ const router = express.Router();
 router.get('/get-academicLoad/export/excel',
             authenticate,
             authorizeRole([RoleEnum.COORDINATOR]),
+            checkActiveProcessByTypeIdMiddleware(5),
             exportExcel
 );
 
 router.get('/get-academicLoad/export/pdf',
     authenticate,
     authorizeRole([RoleEnum.COORDINATOR]),
+    checkActiveProcessByTypeIdMiddleware(5),
     exportPdf
 );
 
